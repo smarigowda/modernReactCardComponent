abd63af1534ee2e042a586d404afaa3a492023d4
Santosh Marigowda <santosharakere@gmail.com>
2019-04-07 20:07:48 +0100

    style

--- src/components/ApprovalCard.js
+++ src/components/ApprovalCard.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const ApprovalCard = ({ children }) => {
+    console.log(children);
     return (
         <div className="approval-card">
+            <h2>Warning !</h2>
+            <h1 className="confirm">Are you sure ?</h1>
             {children}
             <div className="buttons-container">
                 <button type="text" className="button approve">Approve</button>
