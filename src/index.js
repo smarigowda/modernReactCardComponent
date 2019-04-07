99f9d6de9ff5c70cc389c23a35037f7ef4099ee6
Santosh Marigowda <santosharakere@gmail.com>
2019-04-07 07:55:05 +0100

    three cards, copy paste

--- src/index.js
+++ src/index.js
@@ -14,11 +14,35 @@ const App = () => {
                     <img className="avatar-image" src="/avatar-1.jpg" alt="avatar image" ></img>
                 </a>
                 <div className="content">
-                    <a className="author" href="/">Santosh M</a>
                     <div className="metadata">
+                        <a className="author" href="/">Santosh Marigowda</a>
                         <span className="date"> Today at 6 PM</span>
                     </div>
-                    <div className="text"> Nice blog post !</div>
+                    <div className="text">Nice blog post !</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar-container">
+                    <img className="avatar-image" src="/avatar-2.png" alt="avatar image" ></img>
+                </a>
+                <div className="content">
+                    <div className="metadata">
+                        <a className="author" href="/">Roopa Santosh</a>
+                        <span className="date">Yesterday at 2 PM</span>
+                    </div>
+                    <div className="text">Excellent blog post !</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar-container">
+                    <img className="avatar-image" src="/avatar-3.png" alt="avatar image" ></img>
+                </a>
+                <div className="content">
+                    <div className="metadata">
+                        <a className="author" href="/">Samarth Gowda</a>
+                        <span className="date">Yesterday at 2 PM</span>
+                    </div>
+                    <div className="text">Amazing post, thanks !</div>
                 </div>
             </div>
         </div>
