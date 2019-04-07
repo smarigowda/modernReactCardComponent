cb7fa452ae80b2e176fc0e3d1c7298f19a9a2cab
Santosh Marigowda <santosharakere@gmail.com>
2019-04-07 07:26:20 +0100

    css and html for a single comment

--- src/index.js
+++ src/index.js
@@ -8,8 +8,19 @@ console.log(ReactDOM);
 const App = () => {
     const text = 'A React App !';
     return (
-        <div className="heading">
-            {text}
+        <div className="container">
+            <div className="comment">
+                <a href="/" className="avatar-container">
+                    <img className="avatar-image" src="/avatar-1.jpg" alt="avatar image" ></img>
+                </a>
+                <div className="content">
+                    <a className="author" href="/">Santosh M</a>
+                    <div className="metadata">
+                        <span className="date"> Today at 6 PM</span>
+                    </div>
+                    <div className="text"> Nice blog post !</div>
+                </div>
+            </div>
         </div>
     )
 }
